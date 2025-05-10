6e1e567fdfdebda4f9462775c3ed8c0a223413e8
dmaf <dmaf.andinofiallos@gmail.com>
2025-05-10 23:33:43 +0200

    creation of Header file, its basic structure layout, and erased template readme file

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,23 @@
-function Header (){
-    return(
-        <h1>DMAF</h1>
-    )
+function Header() {
+  return (
+    <header>
+      <h1>DMAF</h1>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About me</a>
+        </li>
+        <li>
+          <a href="/">Services</a>
+        </li>
+        <li>
+          <a href="/">Contact</a>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
