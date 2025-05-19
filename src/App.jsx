5443de45a53fe26df87c9c6a134383c2e672390c
dmaf <dmaf.andinofiallos@gmail.com>
2025-05-19 23:41:34 +0200

    started the idea for a main screen in the design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header/Header"
+import MainScreen from "./MainScreen/MainScreen"
 
 function App() {
   return(
    <>
    <Header></Header>
+   <MainScreen></MainScreen>
    </> 
   )
 }
---
+++ src/MainScreen/MainScreen.jsx
@@ -0,0 +1,12 @@
+import styles from './MainScreen.module.css'
+
+function MainScreen() {
+  return (
+    <div className="main--container">
+        <h1>Diego Andino</h1>
+        <h3>Software Engineerer</h3>
+    </div>
+  );
+}
+
+export default MainScreen;
