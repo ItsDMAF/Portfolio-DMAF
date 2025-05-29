f1c0a57e0c4061c88ec2389f709601e273c02237
dmaf <dmaf.andinofiallos@gmail.com>
2025-05-29 23:56:01 +0200

    working on the main part of the website

--- src/MainScreen/MainScreen.jsx
+++ src/MainScreen/MainScreen.jsx
@@ -3,6 +3,9 @@ import styles from './MainScreen.module.css'
 function MainScreen() {
   return (
     <div className={styles.maincontainer}>
+      <div className={styles.header}>
+
+      </div>
         <h1>Diego Andino</h1>
         <h3>Software Engineerer</h3>
     </div>
